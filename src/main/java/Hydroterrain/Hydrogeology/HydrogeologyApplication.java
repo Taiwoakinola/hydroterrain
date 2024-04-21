092fbf5cae28a6e8dee9de145a8849cab757a445
unknown <taiwoakinola77@gmail.com>
2024-04-21 09:01:46 -0700

    first commit

---
+++ src/main/java/Hydroterrain/Hydrogeology/HydrogeologyApplication.java
@@ -0,0 +1,13 @@
+package Hydroterrain.Hydrogeology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrogeologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydrogeologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hydroterrain/Hydrogeology/HydrogeologyApplicationTests.java
@@ -0,0 +1,13 @@
+package Hydroterrain.Hydrogeology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrogeologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
